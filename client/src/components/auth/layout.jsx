22547790fa1170d6da34cb7becbe84472d81eed8
Jigs <jigsdominic.ipong@outlook.com>
2024-10-06 15:01:39 +0800

    third commit

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -1,10 +1,18 @@
 import { Outlet } from "react-router-dom";
+import car from "../../assets/car.jpg";
 
 function AuthLayout() {
   return (
     <div className="flex min-h-screen w-full">
-      <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
-        <div className="max-w-md space-y-6 text-center text-primary-foreground">
+      <div
+        className="hidden lg:flex items-center justify-center w-1/2 px-12"
+        style={{
+          backgroundImage: `url(${car})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div className="max-w-md space-y-6 text-center text-primary-foreground bg-black bg-opacity-50 p-5 rounded-lg">
           <h1 className="text-4xl font-extrabold tracking-tight">
             Welcome to ECommerce Shopping
           </h1>
